cea5e5c1ef87e289878593e61391d6d55c2bbcb8
bugbit <ohernandezba@gmail.com>
2021-03-06 01:46:27 +0100

    init commit

---
+++ TVALGBRA.cpp
@@ -0,0 +1,12 @@
+﻿// TVALGBRA.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "TVALGBRA.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
